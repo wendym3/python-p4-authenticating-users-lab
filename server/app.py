ca772bcdd6d56233ab1a6480b2d96d07bf932d00
wendymasese <wendy.masese@student.moringaschool.com>
2024-09-01 19:06:40 +0300

    done

--- server/app.py
+++ server/app.py
@@ -47,10 +47,41 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter(User.username == username).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {}, 401
+    
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response('', 204)
+
+class  Checksession(Resource):
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        return {}, 401
+
+
+
+
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(Checksession, '/check_session')
 
 
 if __name__ == '__main__':
